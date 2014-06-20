8cd079817f534f60d607be8559caa750b886d9ed
simondodson <simondodson@gmail.com>
2014-06-21 04:59:00 +1000

    install sportsnewsla

--- app/controllers/lakers_controller.rb
+++ app/controllers/lakers_controller.rb
@@ -4,10 +4,10 @@ class LakersController < ApplicationController
 
   def twitter
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_KEY"]
-      config.consumer_secret     = ENV["TWITTER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+      config.consumer_key        = ENV["KbuxyRVsrpJv82NbaKmYXA"]
+      config.consumer_secret     = ENV["nqpfMb0vL5qODyxEhXNNib7WyD0O1ySimV9ZzCmsEI"]
+      config.access_token        = ENV["KPueiaLKfuG9Ep77Uu4UnznhpdEGkrqaC9zOYCmQoq"]
+      config.access_token_secret = ENV["sRPl21ZDjdEiVnQU8Fs2OxElbXUzM2T5LrlyZJg9Lef4C"]
     end
     render json: client.user_timeline('lakers')
   end
